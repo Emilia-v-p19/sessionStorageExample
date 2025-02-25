8f9d9a8c2db35c26b39b6c591da46e6b58f5137f
Emilia-v-p19 <emilia@pretsch.dk>
2025-02-25 13:51:31 +0100

    speed js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,33 @@
 // Record this?
 
 // First - store a value using sessionStorage.setItem("key", "value")
+/*sessionStorage.setItem("favColor", "pink");*/
 
 // Now get it and display it somewhere
+/*const currentUserEl = document.querySelector(".currentUser");
+currentUserEl.textContent =sessionStorage.getItem("favColor")*/
 
 // Delete the sessionStorage and the code above and do the main task:
 
 // Get the form
 
+const formEl = document.querySelector("form");
+
+const currentUserEl = document.querySelector(".currentUser");
+currentUserEl.textContent = sessionStorage.getItem("username");
 // Listen for submit event, prevent default, store the first+last name in sessionStorage
+
+formEl.addEventListener("submit",function(e){
+    //stopper fra at resete e er den tidliger event
+    e.preventDefault();
+    const userFirstName = formEl.formFirstName.value
+    //console.log('userFirstName:', userFirstName)
+    const userLastName = formEl.formLastName.value
+    //console.log('userLastNam:', userLastName)
+
+    sessionStorage.setItem("username", userFirstName + userLastName);
+})
+
 
 // Get the .currentUser and store the value of the sessionStorage in there
 
